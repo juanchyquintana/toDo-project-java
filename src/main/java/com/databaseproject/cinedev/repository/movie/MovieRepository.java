bbda3e67b4c0aeda7a101fb2964ada2f50be4b13
Juan Diego Quintana <quintanajuanchy25@gmail.com>
2025-05-08 12:11:15 -0300

    delete: i removed all about movie, because this file are not required in this project

--- src/main/java/com/databaseproject/cinedev/repository/movie/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.databaseproject.cinedev.repository.movie;
-
-import com.databaseproject.cinedev.models.movie.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
